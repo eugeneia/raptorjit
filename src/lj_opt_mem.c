2e659a2ce63ee9b8d968ebc9902ad3f40ce583ef
Mike Pall <mike>
2025-03-11 23:04:30 +0100

    Change handling of nil value markers in template tables.
    Reported by Bernhard M. Wiedemann. #1348 #1155
    Fixes from Peter Cawley, Christian Clason, Lewis Russell.

--- src/lj_opt_mem.c
+++ src/lj_opt_mem.c
@@ -230,7 +230,9 @@ static TRef fwd_ahload(jit_State *J, IRRef xref)
 	  return TREF_PRI(itype2irt(tv));
 	else if (tvisnum(tv))
 	  return lj_ir_knum_u64(J, tv->u64);
-	else if (tvisgcv(tv))
+	else if (tvistab(tv)) /* Template table nil value marker. */
+	  return TREF_NIL;
+	else if (tvisstr(tv))
 	  return lj_ir_kstr(J, strV(tv));
       }
       /* Othwerwise: don't intern as a constant. */
