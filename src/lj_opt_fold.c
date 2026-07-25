854974971a28450dfa533a2780163390bceaa297
Mike Pall <mike>
2026-07-25 20:53:11 +0200

    FFI: Fix widening semantics for 64 bit arithmetic.
    Thanks to Frityet. #1492

--- src/lj_opt_fold.c
+++ src/lj_opt_fold.c
@@ -1101,7 +1101,10 @@ LJFOLDF(simplify_conv_i64_num)
     fins->op2 = ((IRT_I64<<5)|IRT_INT|IRCONV_SEXT);
     return RETRYFOLD;
   } else if ((fleft->op2 & IRCONV_SRCMASK) == IRT_U32) {
-    return fleft->op1;
+    /* Reduce to a zero-extension. */
+    fins->op1 = fleft->op1;
+    fins->op2 = (IRT_I64<<5)|IRT_U32;
+    return RETRYFOLD;
   }
   return NEXTFOLD;
 }
