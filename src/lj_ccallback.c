b41984d5e463fa66b39d189082e5e18ddad8b257
Mike Pall <mike>
2026-08-01 11:38:57 +0200

    FFI: Set cur_L in FFI callback.
    Thanks to David Komárek. #1498

--- src/lj_ccallback.c
+++ src/lj_ccallback.c
@@ -271,6 +271,7 @@ lua_State * lj_ccallback_enter(CTState *cts, void *cf)
     exit(EXIT_FAILURE);
   }
   lj_trace_abort(g);  /* Never record across callback. */
+  setgcref(g->cur_L, obj2gco(L));
   /* Setup C frame. */
   cframe_prev(cf) = L->cframe;
   setcframe_L(cf, L);
